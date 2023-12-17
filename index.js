ab8b0ab3283219ffcbdb6a4e2939d116684c1383
Renata Kiraly <kirrena@gmail.com>
2023-12-17 19:08:31 +0000

    added basic readme structure displayed in a file

--- index.js
+++ index.js
@@ -3,18 +3,76 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
 
-];
+
+// array of questions for user
+const questions = ["What is the title of your project?", "What is the description?", 
+"How to install?", "How to use?", "What license is the project covered under?", 
+"What tests can be performed?", "What is you gitHub username?", "What is your e-mail address?"];
+
+//destructuring array of questions
+const [title, description, installation, usage, license, tests, github, email ] = questions;
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: title,
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: description,
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: installation,
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: usage,
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: license,
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: tests,
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: github,
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: email,
+    },
+  ])
+  .then((response) => {
+    const fileName = 'readme.md';
+    const data = generateMarkdown(response);
+    writeToFile(fileName,data)
+  });
+  
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Readme is ready')
+    )
 }
+
 
 // function to initialize program
 function init() {
-
+    
 }
 
 // function call to initialize program
